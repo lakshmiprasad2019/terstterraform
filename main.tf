de96e1480aaf07802d5765a0b36eab866d994693
lakshmiprasad2019 <madhava.sadm@gmail.com>
2023-09-03 12:21:54 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "Myvpc" {
+  cidr_block = "20.20.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.Myvpc.id
+  cidr_block = "20.20.1.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_instance" "fristec2" {
+  ami           = data.aws_ami.myami.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+}
+data "aws_ami" "myami" {
+  owners = ["amazon"]
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10**"]
+  }
+}
