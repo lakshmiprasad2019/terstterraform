2e04809d3af089970717f4272b1a595c1902ce80
lakshmiprasad2019 <madhava.sadm@gmail.com>
2024-11-29 09:33:00 +0530

    new subjet has added

--- main.tf
+++ main.tf
@@ -33,6 +33,14 @@ resource "aws_subnet" "secondsubnet" {
     Name = "Private"
   }
 }
+resource "aws_subnet" "secondsubnet" {
+  vpc_id     = aws_vpc.Myvpc.id
+  cidr_block = "20.20.10.0/24"
+
+  tags = {
+    Name = "Private_new"
+  }
+}
 resource "aws_subnet" "secondsubnet1" {
   vpc_id     = aws_vpc.Myvpc.id
   cidr_block = "20.20.3.0/24"
