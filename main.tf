37252ee3de08c7a0da1df9d3c3d7fbd685406343
lakshmiprasad2019 <madhava.sadm@gmail.com>
2024-11-30 12:28:26 +0530

    added new subnet

--- main.tf
+++ main.tf
@@ -49,6 +49,14 @@ resource "aws_subnet" "secondsubnet1" {
     Name = "Private1"
   }
 }
+resource "aws_subnet" "secondsubnet_b855" {
+  vpc_id     = aws_vpc.Myvpc.id
+  cidr_block = "20.20.4.0/24"
+
+  tags = {
+    Name = "Private_b882"
+  }
+}
 resource "aws_instance" "fristec2" {
   ami           = data.aws_ami.myami.id
   instance_type = "t2.micro"
